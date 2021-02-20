8027487189a64ba3aab7d90afa8503c712f58646
KimMyeongJun <myeongjun222@naver.com>
2021-02-20 20:41:49 +0900

    pj1

--- test-server/src/user/user.guard.ts
+++ test-server/src/user/user.guard.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 export class UserGuard implements CanActivate {
   constructor(private jwtService : JwtService){}
   
+  //토큰을 이용하여 접근제한여부 판단
   canActivate(context: ExecutionContext){
     const request = context.switchToHttp().getRequest();
     const jwt = request.cookies['jwt'];
