af134be5f90f9e0ce065a77b5ebde4731c12c7e4
KimMyeonJun <myeongjun222@naver.com>
2021-04-14 18:34:10 +0900

    pj1

--- test-client/src/components/wrapper/nav_bar/user_option/AfterLogin.tsx
+++ test-client/src/components/wrapper/nav_bar/user_option/AfterLogin.tsx
@@ -3,16 +3,32 @@ import { user_image_path } from '../../../../path/ImagePath'
 import { UserDropdown } from '../NavBarStyled'
 
 function AfterLogin() {
-    const re = useRef<HTMLDivElement>(null);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const userImageRef = useRef<HTMLImageElement>(null);
     const [dropdown, setDropDown] = useState(false);
-    const toggleDropdown = () => {
-        setDropDown(!dropdown);
-    }
+
     const handleClickOutside = (e : MouseEvent) => {
-        if(re.current && !re.current.contains(e.target as Node)){
-            setDropDown(false);
+        //dropdownRef가 잡혀져 있고, dropdownRef의 자손 dom이 아닐경우
+        //자손 dom을 클릭했을 경우에는 해당 dom에 대한 action이 필요하므로
+        if(dropdownRef.current && !dropdownRef.current.contains(e.target as Node)){
+            if(userImageRef.current?.contains(e.target as Node)) {
+                if(dropdown === true) {
+                    console.log("true");
+                    setDropDown(false);
+                }
+                else {
+                    console.log("false");
+                    setDropDown(true);
+                }
+                console.log("image 클릭");
+            }
+            else {
+                setDropDown(false);
+                console.log("외부 클릭");
+            }
         }
     }
+    console.log("상태 " + dropdown);
     useEffect(() => {
         document.addEventListener("click", handleClickOutside, true);
         return () => {
@@ -22,12 +38,14 @@ function AfterLogin() {
 
     return (
         <ul className="user-option-container">
-            <li><i className="far fa-bell"></i></li>
+            <li><i className="far fa-bell"/></li>
             <li>
-                <img className="user-image" onClick={toggleDropdown} src={user_image_path} alt="user"/>
-                <UserDropdown ref={re} toggle={dropdown} {...dropdown} className="user-menu">
+                <img ref={userImageRef} className="user-image"  src={user_image_path} alt="user"/>
+                <UserDropdown ref={dropdownRef} toggle={dropdown} {...dropdown} className="user-menu">
                     <a href="#">usermenu1</a>
+                    <hr/>
                     <a href="#">usermenu2</a>
+                    <hr/>
                     <a href="#">usermenu3</a>
                 </UserDropdown>
             </li>
