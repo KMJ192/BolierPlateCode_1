a2ec63f58148be445d115cf0652d5d923fdfc8a4
KimMyeonJun <myeongjun222@naver.com>
2021-04-17 22:58:12 +0900

    pj1

--- test-client/src/components/view/user_register/InputBoxList.tsx
+++ test-client/src/components/view/user_register/InputBoxList.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { ErrorMessage } from 'formik';
 
 interface Props{
-    
+    containerCName : string;
+    title : string;
+    id : string
+    placeholder : string;
+    inputType : string;
 }
 
-function InputBoxList() {
+function InputBoxList({
+        containerCName,
+        title,
+        id,
+        placeholder,
+        inputType, 
+    }: Props) {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={containerCName}>
+                <label htmlFor={id}>{title}</label>
+                <br/>
+                <input 
+                    id={id}
+                    type={inputType} 
+                    placeholder={placeholder}
+                />
+                {containerCName === "email-container" && 
+                    <button className="duplicate-confirm">중복확인</button>
+                }
+                {containerCName === "nickname-container" && 
+                    <button className="duplicate-confirm">중복확인</button>
+                }
+            </div>
+            {/* <ErrorMessage 
+                component="div"
+                name={id}
+                className="warn-message"
+            /> */}
+        </>
+        
     )
 }
 
-export default InputBoxList
+export default InputBoxList;
--- test-client/src/components/view/user_register/UserRegisterPage.tsx
+++ test-client/src/components/view/user_register/UserRegisterPage.tsx
@@ -4,20 +4,26 @@ import * as Yup from 'yup';
 import { user_image_path } from '../../../path/ImagePath';
 import Wrapper from '../../wrapper/Wrapper';
 import './UserRegisterPage.scss';
+import InputBoxList from './InputBoxList';
 
 function UserRegisterPage() {
     document.title="회원가입";
     const [userimgBase64, setUserimgBase64] = useState(user_image_path);
 
     const validate = Yup.object({
-        email: Yup.string().email("이메일 양식에 맞춰주세요"),
+        email: Yup.string()
+            .email("이메일 양식에 맞춰주세요")
+            .required("이메일을 작성해주세요"),
         nickname: Yup.string()
-            .min(2, "별명은 2글자 이상입니다.")
-            .max(20, "20자 내로 작성해주세요."),
+            .min(2, "별명은 2글자 이상입니다")
+            .max(20, "20자 내로 작성해주세요")
+            .required("별명을 작성해주세요"),
         password : Yup.string()
-            .min(6, "비밀번호는 최소 6자입니다"),
+            .min(6, "비밀번호는 최소 6자입니다")
+            .required("비밀번호를 작성해주세요"),
         password_confirm : Yup.string()
-            .oneOf([Yup.ref("password"), null], "비밀번호 확인과 다릅니다.")
+            .oneOf([Yup.ref("password"), null], "비밀번호 확인과 다릅니다")
+            .required("비밀번호확인을 작성해주세요")
     });
 
     const formik = useFormik({
@@ -46,8 +52,37 @@ function UserRegisterPage() {
                 </div>
                 <div className="user-info-container">
                     <div className="user-data-des">가입정보</div>
+                    <InputBoxList 
+                        containerCName="email-container"
+                        title="이메일 주소"
+                        id="email-box"
+                        placeholder="이메일 입력"
+                        inputType="text"
+                    />
+                    <InputBoxList 
+                        containerCName="nickname-container"
+                        title="별명"
+                        id="nickname-box"
+                        placeholder="별명 입력"
+                        inputType="text"
+                    />
+                    <InputBoxList 
+                        containerCName="password-container"
+                        title="비밀번호"
+                        id="password-box"
+                        placeholder="비밀번호 입력"
+                        inputType="password"
+                    />
+                    <InputBoxList 
+                        containerCName="password-container"
+                        title="비밀번호 확인"
+                        id="password-confirm-box"
+                        placeholder="비밀번호 확인"
+                        inputType="password"
+                    />
+                    {/* 
                     <div className="email-container">
-                        <div>이메일 주소</div>
+                        <div>이메일</div>
                         <input type="text" placeholder="이메일 입력"/>
                     </div>
                     <div className="nickname-container">
@@ -57,9 +92,11 @@ function UserRegisterPage() {
                     <div className="password-container">
                         <div>비밀번호</div>
                         <input type="password" placeholder="비밀번호 입력"/>
+                    </div>
+                    <div className="password-container">
                         <div>비밀번호 확인</div>
                         <input type="password" placeholder="비밀번호 확인"/>
-                    </div>
+                    </div> */}
                     <div className="btn-container">
                         <button type="submit">가입하기</button>
                         <button type="button">로그인</button>
