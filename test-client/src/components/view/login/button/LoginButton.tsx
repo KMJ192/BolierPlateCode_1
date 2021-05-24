db0d27b0bbb1a415f31d8297642145943c97ab23
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-05-25 00:54:37 +0900

    pj1

--- test-client/src/components/view/login/button/LoginButton.tsx
+++ test-client/src/components/view/login/button/LoginButton.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../redux-module/RootReducer';
+import { StyledLoginButton } from '../LoginPageStyle';
 
-function LoginButton() {
+interface Props{
+    type: "button" | "submit";
+    className: string;
+    setData : () => void
+}
+
+const defualtButtonWidth: number = 325;
+const diffButtonWidth: number = 120;
+
+function LoginButton( { type, className, setData} : Props) {
+    const [winWidth, setWinWidth] = useState(defualtButtonWidth);
+    
+    const windowSize = useSelector((state : RootState) => state.screen_size);
+    useEffect(() => {
+        if(windowSize.width < 435 
+            && windowSize.width > 245
+            && winWidth !== (windowSize.width - diffButtonWidth)){
+                setWinWidth(windowSize.width - diffButtonWidth);
+            }
+        if(windowSize.width >= 435
+            && winWidth !== defualtButtonWidth){
+                setWinWidth(defualtButtonWidth);
+            }
+    }, [winWidth, windowSize.width]);
+
     return (
-        <div>
-            
-        </div>
+        <StyledLoginButton
+            className={className}
+            type={type}
+            onClick={setData}
+        />
     )
 }
 
